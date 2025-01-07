0806fa99b3e83875830f41ea6dcb57ccc9e62100
rosameliacarioni <r.carioniporras@student.maastrichtuniversity.nl>
2025-01-07 21:55:40 +0100

    training pipeline done!

--- training_pipeline/train.py
+++ training_pipeline/train.py
@@ -1,6 +1,142 @@
+import argparse
+from feature_pipeline.ETL import load
+from utils import data
+from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+import os 
+import hopsworks
+from utils.settings import ENV_VARS
+from hsml.schema import Schema
+from hsml.model_schema import ModelSchema
 
-# hyperparameter tuning 
 
-# train 
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', '-v', type=int, default=1, help='Version for the feature groups.')
+    parser.add_argument('--hyperparameter_tuning', '-ht', default=False, action='store_true', help='Decides if hyperparametertuning is performed or not.')
+    parser.add_argument("--model_name", type=str, default='model_total_production', help='Name given when saving the model both locally and in Hopsworks.')
+    # Mutually exclusive group: backfill (-b) or daily (-d) is required
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--total_production', '-tp', action='store_true', help='Defines how many and which features are used during training. In this case only the total production of energy is used.')
+    group.add_argument('--all_production', '-ap', action='store_true', help='Defines how many and which features are used during training. In this case all types of energy produced per country are considered.')
+    return parser
 
-# evaluate 
+
+def get_training_data(version:int, total_production:bool):
+    feature_store = load.get_feature_store()
+
+    feature_view = feature_store.get_feature_view(
+        name = 'cross_border_electricity_fv',
+        version = version
+    )
+    X_train, X_test, y_train, y_test = data.split_training_data(feature_view)
+    X_train_one_hot, X_test_one_hot = data.prepare_data(X_train, X_test, total_production) 
+    return X_train_one_hot, X_test_one_hot, y_train, y_test
+
+
+def train(hyperparameter_tuning:bool, X_train_one_hot, y_train):
+    # Define the model
+    xgb_regressor = XGBRegressor()
+    if hyperparameter_tuning:
+        # Define the hyperparameter grid
+        param_grid = {
+            'n_estimators': [100, 200, 300],
+            'max_depth': [3, 5, 6, 7, 10, 20],
+            'learning_rate': [0.01, 0.05, 0.1, 0.2, 0.3],
+            'subsample': [0.8, 0.9, 1.0],
+            'colsample_bytree': [0.8, 0.9, 1.0]
+        }
+
+        grid_search = GridSearchCV(
+            estimator=xgb_regressor,
+            param_grid=param_grid,
+            scoring='neg_mean_squared_error',
+            cv=3,  # 3-fold cross-validation
+            verbose=2,
+            n_jobs=-1  # Use all available cores
+        )
+
+        # Perform hyperparameter tuning
+        grid_search.fit(X_train_one_hot, y_train)
+
+        # Retrieve the best model
+        best_model = grid_search.best_estimator_
+        return best_model 
+    else: 
+        xgb_regressor = XGBRegressor()
+        xgb_regressor.fit(X_train_one_hot, y_train)
+        return xgb_regressor
+
+def evaluate(model, x_test, y_test): 
+    y_pred = model.predict(x_test)
+    # Calculating Mean Squared Error (MSE) using sklearn
+    mse = mean_squared_error(y_test.iloc[:,0], y_pred)
+    print("MSE:", mse)
+
+    # Calculating R squared using sklearn
+    r2 = r2_score(y_test.iloc[:,0], y_pred)
+    print("R squared:", r2)
+    return {"MSE": str(mse), "R squared": str(r2)}
+
+def save_model(model, locally:bool, model_name:str, to_hopsworks:bool, results_dict:dict, X_train, y_train, X_test):
+    model_dir = "models"
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+    if locally:
+        model.save_model(model_dir + f"/{model_name}.json")
+    if to_hopsworks:
+        project = hopsworks.login(api_key_value=ENV_VARS["HOPSWORKS"], project='cross_border_electricity')
+        mr = project.get_model_registry()
+
+        input_schema = Schema(X_train)
+        output_schema = Schema(y_train)
+        model_schema = ModelSchema(input_schema=input_schema, output_schema=output_schema)
+
+        # Creating a Python model in the model registry 
+        cross_border_model = mr.python.create_model(
+            name=model_name, 
+            metrics= results_dict,
+            model_schema=model_schema,
+            input_example=X_test.sample().values, 
+            description="Trained XGBRegressor model for predicting cross-border electricity flows.",
+        )
+        # Saving the model artifacts in the model registry
+        cross_border_model.save(model_dir)
+
+
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    version = args.version
+    total_production = args.total_production
+    hyperparameter_tuning = args.hyperparameter_tuning
+    model_name = args.model_name
+
+    # Train the model
+    print("Starting training...")
+    if args.total_production:
+        X_train_one_hot, X_test_one_hot, y_train, y_test = get_training_data(version, True)
+    else:
+        X_train_one_hot, X_test_one_hot, y_train, y_test = get_training_data(version, False)
+    
+    model = train(hyperparameter_tuning, X_train_one_hot, y_train)
+
+    # Evaluate the model
+    print("Evaluating the model...")
+    results_dict = evaluate(model, X_test_one_hot, y_test)
+
+    # Save the model     
+    save_model(
+        model=model,
+        locally=True,
+        model_name=model_name,
+        to_hopsworks=True,
+        results_dict=results_dict,
+        X_train=X_train_one_hot,
+        y_train=y_train,
+        X_test=X_test_one_hot
+    )
+    print(f"Model saved locally and to Hopsworks with name '{model_name}'.") 
--- utils/data.py
+++ utils/data.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime, timedelta
+import hsfs.feature_view
 import pandas as pd
 import matplotlib.pyplot as plt
 from xgboost import XGBRegressor
@@ -7,6 +8,7 @@ from xgboost import plot_importance
 from sklearn.metrics import mean_squared_error, r2_score
 from feature_pipeline.ETL import load, transform
 import hopsworks
+import hsfs
 
 
 BZN2COUNTRY = {
@@ -25,6 +27,90 @@ NEIGHBOUR_ZONES = {
 ZONES = {
     'NL', 'BE', 'DE_LU', 'GB', 'NO_2', 'DK_1'
 }
+
+COLUMNS_MODEL_TOTAL_PRODUCTION = [
+    'datetime',
+    'country_from',
+    'country_to',
+    'cloudcover_be',
+    'cloudcover_de_lu',
+    'cloudcover_dk_1',
+    'cloudcover_gb',
+    'cloudcover_nl',
+    'cloudcover_no_2',
+    'diffuse_radiation_be',
+    'diffuse_radiation_de_lu',
+    'diffuse_radiation_dk_1',
+    'diffuse_radiation_gb',
+    'diffuse_radiation_nl',
+    'diffuse_radiation_no_2',
+    'direct_radiation_be',
+    'direct_radiation_de_lu',
+    'direct_radiation_dk_1',
+    'direct_radiation_gb',
+    'direct_radiation_nl',
+    'direct_radiation_no_2',
+    'precipitation_be',
+    'precipitation_de_lu',
+    'precipitation_dk_1',
+    'precipitation_gb',
+    'precipitation_nl',
+    'precipitation_no_2',
+    'snow_depth_be',
+    'snow_depth_de_lu',
+    'snow_depth_dk_1',
+    'snow_depth_gb',
+    'snow_depth_nl',
+    'snow_depth_no_2',
+    'surface_pressure_be',
+    'surface_pressure_de_lu',
+    'surface_pressure_dk_1',
+    'surface_pressure_gb',
+    'surface_pressure_nl',
+    'surface_pressure_no_2',
+    'temperature_2m_be',
+    'temperature_2m_de_lu',
+    'temperature_2m_dk_1',
+    'temperature_2m_gb',
+    'temperature_2m_nl',
+    'temperature_2m_no_2',
+    'wind_direction_100m_be',
+    'wind_direction_100m_de_lu',
+    'wind_direction_100m_dk_1',
+    'wind_direction_100m_gb',
+    'wind_direction_100m_nl',
+    'wind_direction_100m_no_2',
+    'wind_direction_10m_be',
+    'wind_direction_10m_de_lu',
+    'wind_direction_10m_dk_1',
+    'wind_direction_10m_gb',
+    'wind_direction_10m_nl',
+    'wind_direction_10m_no_2',
+    'wind_speed_100m_be',
+    'wind_speed_100m_de_lu',
+    'wind_speed_100m_dk_1',
+    'wind_speed_100m_gb',
+    'wind_speed_100m_nl',
+    'wind_speed_100m_no_2',
+    'wind_speed_10m_be',
+    'wind_speed_10m_de_lu',
+    'wind_speed_10m_dk_1',
+    'wind_speed_10m_gb',
+    'wind_speed_10m_nl',
+    'wind_speed_10m_no_2',
+    'energy_price_be',
+    'energy_price_de_lu',
+    'energy_price_dk_1',
+    'energy_price_gb',
+    'energy_price_nl',
+    'energy_price_no_2',
+    'total_generation_be',
+    'total_generation_de_lu',
+    'total_generation_dk_1',
+    'total_generation_gb',
+    'total_generation_nl',
+    'total_generation_no_2'
+]
 
 
 def create_feature_view(version: int = 1):
@@ -52,7 +138,49 @@ def _retrieve_feature_groups(version: int = 1):
     return weather_fg, prices_generation_fg, physical_flow_fg
 
 
-# load dataset: either for training or inference 
+def prepare_data(X_train: pd.DataFrame, X_test: pd.DataFrame, total_production: bool):
+    X_train_result = X_train.copy()
+    X_test_result = X_test.copy()
+
+    # Remove not needed columns 
+    if total_production:
+        X_train_result = X_train_result[COLUMNS_MODEL_TOTAL_PRODUCTION]
+        X_test_result = X_test_result[COLUMNS_MODEL_TOTAL_PRODUCTION]
+    else:
+        columns_to_drop = ['total_generation_nl', 'total_generation_be', 'total_generation_de_lu', 'total_generation_dk_1', 'total_generation_gb',  'total_generation_no_2']
+        X_train_result = X_train_result.drop(columns = columns_to_drop)
+        X_test_result = X_test_result.drop(columns = columns_to_drop)
+    
+    # Remove datetime column
+    X_train_result = X_train_result.drop(columns = ['datetime'])
+    X_test_result = X_test_result.drop(columns = ['datetime'])
+
+    # Apply one-hot encoding with prefixes
+    X_train_result = one_hot_encoding(X_train_result, 'country_from', prefix='from')
+    X_train_result = one_hot_encoding(X_train_result, 'country_to', prefix='to')   
+    X_test_result = one_hot_encoding(X_test_result, 'country_from', prefix='from')
+    X_test_result = one_hot_encoding(X_test_result, 'country_to', prefix='to')  
+ 
+    return X_train_result, X_test_result
 
 
-# prepare data (train test split): training_pipeline 
+def one_hot_encoding(df, feature, prefix=None):
+    """
+    Performs one-hot encoding on the specified feature and ensures the result is 1 and 0.
+    Adds a prefix to the columns to avoid name collisions.
+    """
+    df_result = df.copy()
+    # Generate one-hot encoding
+    df_one_hot = pd.get_dummies(df[feature], prefix=prefix or feature)
+    # Convert to integers to ensure 1 and 0
+    df_one_hot = df_one_hot.astype(int)
+    # Drop the original feature column and join one-hot encoding
+    df_result = df_result.drop(columns=[feature], axis=1)
+    df_result = df_result.join(df_one_hot)
+    return df_result
+
+
+def split_training_data(feature_view: hsfs.feature_view.FeatureView,
+                        test_start: pd.Timestamp = pd.Timestamp('2024-01-01', tz='UTC').normalize()):
+    X_train, X_test, y_train, y_test = feature_view.train_test_split(test_start=test_start)
+    return X_train, X_test, y_train, y_test
